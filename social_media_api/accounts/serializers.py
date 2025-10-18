0ddd8c1cb1122fed1b6d3f77138e12eeb642374e
Muna Faith <munafaith294@gmail.com>
2025-10-18 16:21:14 +0300

    feat: Initial setup with custom user model and token auth

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,30 +1,29 @@
+# accounts/serializers.py
+
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token 
-
-# Get the custom user model using Django's recommended function
-User = get_user_model()
+from rest_framework.authtoken.models import Token
 
 class UserSerializer(serializers.ModelSerializer):
     """
     Serializer for user registration that also handles token creation.
     """
-    email = serializers.EmailField(required=False)
-    token = serializers.CharField(read_only=True) # Field to return the token
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    token = serializers.CharField(read_only=True)
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['username', 'email', 'password', 'bio', 'profile_picture', 'token']
-        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             password=validated_data['password'],
             email=validated_data.get('email', ''),
             bio=validated_data.get('bio', '')
         )
         token = Token.objects.create(user=user)
-        # Add the token to the instance so it can be returned in the response
         user.token = token.key
         return user
+    
+    
